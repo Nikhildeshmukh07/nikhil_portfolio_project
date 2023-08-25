6dc10bcdabf22e8fa3c0a47fe4978381949b2670
nick00000007 <deshnick89@gamil.com>
2023-08-25 19:31:23 +0530

    commit home

--- lib/Myhome.dart
+++ lib/Myhome.dart
@@ -4,6 +4,23 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: PopupMenuButton
+        (
+          color: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(7)
+          ),
+          itemBuilder: (context) => [
+
+        ],),
+      ),
+
+    );
   }
 }
